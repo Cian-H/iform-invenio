f4ae95a261254fb8d4d3601a9f96945978799122
Cian Hughes <chughes000@gmail.com>
2025-02-10 17:16:16 +0000

    Updated jq query to output strings

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 aws secretsmanager get-secret-value --secret-id Invenio | \
-  jq -r '.SecretString | fromjson | to_entries | .[] | .key + "=" + .value' > secrets.env
+  jq -r '.SecretString | fromjson | to_entries | .[] | .key + "=\"" + .value + "\""' > secrets.env
 cat am-d-model.env secrets.env > .env
