6e2e1c30fc8b58d59e7d162144ab1553f7debe7d
Cian Hughes <chughes000@gmail.com>
2025-02-11 00:58:37 +0000

    Added prettier formatting

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,15 @@ const config = {
     tabWidth: 4,
     trailingComma: "all",
     printWidth: 100,
+    plugins: ["prettier-plugin-svelte"],
+    overrides: [
+        {
+            files: "*.svelte",
+            options: {
+                parser: "svelte",
+            },
+        },
+    ],
 };
 
 export default config;
