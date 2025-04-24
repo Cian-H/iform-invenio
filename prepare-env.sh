e87b63774b82a1c5dd96d2cbb1f5154845703166
Cian Hughes <chughes000@gmail.com>
2025-04-24 11:04:12 +0100

    Updated env preparation

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+if [[ "$*" == *--dev* ]]; then
+  ENV_FILE="invenio_dev.env"
+else
+  ENV_FILE="invenio.env"
+fi
+
 aws secretsmanager get-secret-value --secret-id Invenio | \
   jq -r '.SecretString | fromjson | to_entries | .[] | .key + "=\"" + .value + "\""' > secrets.env
-cat invenio.env secrets.env > .env
+
+cat "$ENV_FILE" secrets.env > .env
+
+echo "Environment set up using $ENV_FILE"
