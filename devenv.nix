884967a7aed8b9f1b69dfac5382a13ff7560e325
Cian Hughes <chughes000@gmail.com>
2026-06-29 18:27:27 +0100

    Fixed configuration for 13.0

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,8 @@
 }: {
   packages = with pkgs; [
     awscli2
+    cairo
+    cairomm
     git
     jq
     nodejs
@@ -18,11 +20,20 @@
   languages = {
     python = {
       enable = true;
+      version = "3.12";
       uv.enable = true;
     };
     javascript.npm = {
       enable = true;
       install.enable = true;
     };
+  };
+
+  env = {
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [
+      cairo
+      cairomm
+    ]);
+    NIX_LD = lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
   };
 }
